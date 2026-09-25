07d44519f23b68e000583ecf4ecb3490a1c63e04
Huzaifa Manzar <huzaifamanzar45@gmail.com>
2026-09-25 04:52:23 -0700

    UI work done

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/form', function () {
+    return view('form');
+});
 
 Route::middleware([
     'auth:sanctum',
